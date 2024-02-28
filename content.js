ad9a98bc4b702ab8ec1d230a4eb8166bd9ccd43a
TriangleSnake <trianglesnake2002@gmail.com>
2024-02-28 15:30:45 +0800

    Finish project

---
+++ content.js
@@ -0,0 +1,25 @@
+var name = document.getElementsByClassName('x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f');
+var people = document.getElementsByClassName("_b5a");
+var person;
+var people_lst = [];
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+async function clickMatchingPeople() {
+  while (true) {
+    for (let i = 4; i < 10; i++) {
+        const person = people[i];
+        const person_name = person.querySelector('.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.xt0b8zv.xzsf02u.x1s688f').textContent;
+
+        if (people_lst.includes(person_name)) {
+            try{
+                person.querySelector('.x1i10hfl.xjbqb8w.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x972fbf.xcfux6l.x1qhh985.xm0m39n.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x87ps6o.x1lku1pv.x1a2a7pz.x9f619.x3nfvp2.xdt5ytf.xl56j7k.x1n2onr6.xh8yej3').click();
+            }
+            catch(error){}
+        }
+    }
+    await sleep(1000);
+  }
+}
+
+clickMatchingPeople();
